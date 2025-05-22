85c4602daaabca625c4f339a179e557247202cc5
psukonkin <psukonkin@phoenixit.ru>
2025-05-22 13:44:46 +0300

    fix

--- dist/components/Button/Button.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-export interface IButtonProps {
-    children: React.ReactNode;
-}
-export declare const Button: ({ children, ...props }: IButtonProps) => import("react/jsx-runtime").JSX.Element;
