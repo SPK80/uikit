33f6c911083a6d950e53ae8dc2da1696a75bac75
psukonkin <psukonkin@phoenixit.ru>
2025-05-26 17:35:16 +0300

    update config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,59 +1,89 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import postcss from 'rollup-plugin-postcss';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import dts from 'rollup-plugin-dts';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
 import path from "path";
 import fs from "fs";
 
-// import packageJson from "./package.json";
-
-
 const getComponents = () => {
-  const componentsDir ='./src/components'
-  console.log(componentsDir)
+  const componentsDir = "./src/components";
   return fs
-      .readdirSync(componentsDir)
-      .filter((file) =>
-          fs.statSync(path.join(componentsDir, file)).isDirectory()
-      );
+    .readdirSync(componentsDir)
+    .filter((file) =>
+      fs.statSync(path.join(componentsDir, file)).isDirectory(),
+    );
 };
 
+const components = getComponents();
+
+// обновляет exports в package.json
+const updateExports = () => {
+  const exportsMap = {
+    ".": {
+      import: "./dist/index.es.js",
+      require: "./dist/index.cjs",
+      types: "./dist/index.d.ts",
+    },
+  };
+  components.forEach((name) => {
+    exportsMap[`./components/${name}`] = {
+      import: `./dist/components/${name}.es.js`,
+      require: `./dist/components/${name}.cjs`,
+      types: `./dist/components/${name}.d.ts`,
+    };
+  });
+
+  const packageJsonPath = path.resolve(".", "package.json");
+  let packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+  packageJson.exports = exportsMap;
+  fs.writeFileSync(
+    packageJsonPath,
+    JSON.stringify(packageJson, null, 2),
+    "utf8",
+  );
+
+  console.log("Exports updated in package.json");
+};
 
 const createComponentConfig = (name) => {
-  return [{
-    input: `src/components/${name}/index.ts`,
-        output: [
-    { file: `dist/components/${name}.cjs`, format: 'cjs' },
-    { file: `dist/components/${name}.es.js`, format: 'es' },
-  ],
-    plugins: [
-    peerDepsExternal(),
-    typescript(),
-    resolve(),
-    commonjs(),
-    postcss({
-      extract: true,
-      minimize: true,
-      inject: false,
-    }),
-  ],
-  },
-  {
-    input: `src/components/${name}/index.ts`,
-        output: [{ file: `dist/components/${name}.d.ts`, format: 'es' }],
-      plugins: [dts()]
-  }]
-}
+  return [
+    {
+      input: `src/components/${name}/index.ts`,
+      output: [
+        { file: `dist/components/${name}.cjs`, format: "cjs" },
+        { file: `dist/components/${name}.es.js`, format: "es" },
+      ],
+      plugins: [
+        peerDepsExternal(),
+        typescript(),
+        resolve(),
+        commonjs(),
+        postcss({
+          extract: true,
+          minimize: true,
+          inject: false,
+        }),
+      ],
+    },
+    {
+      input: `src/components/${name}/index.ts`,
+      output: [{ file: `dist/components/${name}.d.ts`, format: "es" }],
+      plugins: [dts()],
+    },
+  ];
+};
+
+updateExports();
 
 export default [
   // === Основная библиотека: uikit ===
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
-      { file: 'dist/index.cjs', format: 'cjs' },
-      { file: 'dist/index.es.js', format: 'es' },
+      { file: "dist/index.cjs", format: "cjs" },
+      { file: "dist/index.es.js", format: "es" },
     ],
     plugins: [
       peerDepsExternal(),
@@ -73,39 +103,10 @@ export default [
       }),
     ],
   },
-
-  // === Компонент Button: uikit/components/Button ===
-  // {
-  //   input: 'src/components/Button/index.ts',
-  //   output: [
-  //     { file: 'dist/components/Button.cjs', format: 'cjs' },
-  //     { file: 'dist/components/Button.es.js', format: 'es' },
-  //   ],
-  //   plugins: [
-  //     typescript(),
-  //     resolve(),
-  //     commonjs(),
-  //     postcss({
-  //       extract: true,
-  //       minimize: true,
-  //       inject: false,
-  //     }),
-  //   ],
-  //   external: ['react', 'react-dom'],
-  // },
-
-  // === Типы для основной библиотеки ===
   {
-    input: 'src/index.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'es' }],
-    plugins: [dts()]
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
   },
-
-  // === Типы для Button ===
-  // {
-  //   input: 'src/components/Button/index.ts',
-  //   output: [{ file: 'dist/components/Button.d.ts', format: 'es' }],
-  //   plugins: [dts()]
-  // }
-    ...getComponents().flatMap(createComponentConfig)
+  ...components.flatMap(createComponentConfig),
 ];
